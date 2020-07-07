9649d7a60718d9568f937cd6f1304a1cc283375c
Jeffrey Harding <jeffreyh202@gmail.com>
2020-07-07 11:33:28 -0600

    final corrections js html

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,15 @@
 
   //Variables
 var submit = document.querySelector("#submit");
-var ZIP = document.querySelector("#zip").value[""];
+var ZIP = document.querySelector("#zip");
 var APIKey = "777c3cb80e0d9493508e59afe3736831";
-var queryURL = `https://api.openweathermap.org/data/2.5/weather?zip=${ZIP}&appid=` + APIKey;
+
 // var queryURL = "https://api.openweathermap.org/data/2.5/weather?zip=84040&appid=" + APIKey;
 
 
 $("#submit").on("click", function() {
-  console.log(ZIP)
+  console.log(ZIP.value)
+  var queryURL = `https://api.openweathermap.org/data/2.5/weather?zip=${ZIP.value}&appid=` + APIKey;
     //AJAX call
    $.ajax({
     url: queryURL,
@@ -17,9 +18,9 @@ $("#submit").on("click", function() {
 
     var K = (response.main.temp);
     var F = Math.trunc((K - 273.15) * 1.80 + 32);
-    $(".wind").append(response.wind.speed);
-    $(".humidity").append(response.main.humidity);
-    $(".temp").append(F);
+    $(".wind").empty().append(response.wind.speed);
+    $(".humidity").empty().append(response.main.humidity);
+    $(".temp").empty().append(F);
 
   });
 });
